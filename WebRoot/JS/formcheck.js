ee722acf8d764c23e79a7ebf3c05f45842bcdfc6
chenshiyu <chenshiyu79@163.com>
2014-05-15 23:58:10 +0800

    修改密码的功能

--- WebRoot/JS/formcheck.js
+++ WebRoot/JS/formcheck.js
@@ -1,3 +1,4 @@
+//添加新用户的表单确认
 function formcheck() {
 	if (document.myform.account.value.trim() == "") {
 		alert("用户名不能为空，请输入用户名！");
@@ -28,4 +29,39 @@ function formcheck() {
 		document.myform.password.focus();
 		return false;
 	}
+}
+
+//修改密码的表单确认
+function modifypwdformcheck() {
+	
+	if (document.myform.password.value.trim() == "") {
+		alert("原密码不能为空，请输入密码！");
+		document.myform.password.focus();
+		return false;
+	}
+	
+	if (document.myform.newpassword.value.trim() == "") {
+		alert("新密码不能为空，请输入密码！");
+		document.myform.newpassword.focus();
+		return false;
+	}
+	
+	if (document.myform.newpassword2.value.trim() == "") {
+		alert("请确认密码！");
+		document.myform.newpassword2.focus();
+		return false;
+	}
+	
+	if (document.myform.newpassword.value.trim() != document.myform.newpassword2.value.trim()) {
+		alert("两次密码输入不一致，请重新输入密码！");
+		document.myform.newpassword2.focus();
+		return false;
+	}
+	
+	if (document.myform.newpassword.value.trim() == document.myform.password.value.trim()) {
+		alert("新密码与原有密码一致，请重新输入新密码！");
+		document.myform.newpassword.focus();
+		return false;
+	}
+	
 }
--- src/com/friday/service/UserService.java
+++ src/com/friday/service/UserService.java
@@ -5,5 +5,5 @@ import com.friday.model.User;
 public interface UserService {
 	public int addUser(User user) throws Exception;
 	public User getUser(String account) throws Exception;
-	public  int updateByPrimaryKeySelective(User record);
+	public  int updateByPrimaryKey(String uid, String password, String newpassword) throws Exception;
 }
